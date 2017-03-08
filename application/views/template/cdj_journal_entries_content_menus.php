ae96b794bb6c247ca38561d94831a95246202a8d
John Kenneth Tolentino <tolentinojohnkenneth007@gmail.com>
2017-03-08 14:07:58 +0800

    Cash Voucher Printout

--- application/views/template/cdj_journal_entries_content_menus.php
+++ application/views/template/cdj_journal_entries_content_menus.php
@@ -11,6 +11,7 @@
     <div class="col-lg-12">
         <div class="title-action" style="margin-left: 3%;">
             <a href="Templates/layout/journal-cdj?id=<?php echo $journal_info->journal_id; ?>&type=preview" target="_blank" class="btn btn-default" style="text-transform:none;font-family: tahoma;" ><i class="fa fa-print"></i> Print </a>
+            <a href="Templates/layout/journal-cdj?id=<?php echo $journal_info->journal_id; ?>&type=cash-voucher" target="_blank" class="btn btn-default" style="text-transform:none;font-family: tahoma;"><i class="fa fa-file-text-o"></i> Print Check Voucher</a>
             <a href="Templates/layout/journal-cdj?id=<?php echo $journal_info->journal_id; ?>&type=pdf" class="btn btn-default" style="text-transform:none;font-family: tahoma;" ><i class="fa fa-file-pdf-o"></i> Download as PDF </a>
         </div>
     </div>
